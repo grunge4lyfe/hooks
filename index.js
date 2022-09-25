9441991531395a704fee14c521a7bff37f6a9bd3
DelightedCat <git@delightedcat.net>
2022-09-25 23:06:05 +0200

    Add User-Agent spoofing

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = require('express')();
 require('dotenv').config();
 const PORT = process.env.PORT || 3030;
 
-app.use('/', proxy('https://discord.com'));
+app.use('/', proxy('https://discord.com', {
+    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+        proxyReqOpts.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0';
+        return proxyReqOpts;
+    }
+}));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port *:${PORT}`);
